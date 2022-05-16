78fd632cc6d110ab6e7ad1f964a16830ea56710b
André Pestana Silva <atlas.ifpa@gmail.com>
2022-05-16 17:18:09 -0300

    Adicionando enunciados das questões

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/agora-a-pratica.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/agora-a-pratica.js
@@ -1,3 +1,16 @@
+/*
+  Enunciados das questões:
+
+  1.Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+  2.Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+  3.Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+    * A média aritmética é o resultado da soma de todos os elementos divido pelo número total de   elementos.
+  4.Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+  5.🚀 Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+  6.Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+  7.Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+*/
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum  = 0, bigger = 0, even = 0;
 let less = numbers[0];
