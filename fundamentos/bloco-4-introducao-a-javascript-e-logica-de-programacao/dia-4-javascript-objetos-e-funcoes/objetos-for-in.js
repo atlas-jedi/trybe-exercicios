4a365c3d8e3e98a866a56ac584b6390895415522
André Pestana Silva <atlas.ifpa@gmail.com>
2022-05-20 09:22:12 -0300

    Resolvendo os exercícios 1 e 2 da Primeira Parte

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos-for-in.js
@@ -0,0 +1,13 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  // Exercício 2
+  recorrente: 'Sim',
+};
+
+// Exercício 1
+console.log(`Bem-Vinda, ${info.personagem}`);
+
+// Exercício 2
+console.log(info);
