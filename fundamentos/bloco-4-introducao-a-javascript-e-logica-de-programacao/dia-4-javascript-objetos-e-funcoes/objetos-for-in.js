dbe15d129cbc0d2b234beaca7eea0c17d734387d
André Pestana Silva <atlas.ifpa@gmail.com>
2022-05-23 13:25:16 -0300

    Resolvendo os exercícios 4 e 5 da primeira parte

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos-for-in.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos-for-in.js
@@ -16,3 +16,41 @@ console.log(info);
 for (propriedade in info) {
   console.log(propriedade);
 }
+
+// Exercício 4
+for (propriedade in info) {
+  console.log(info[propriedade]);
+}
+
+// Exercício 5
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178'",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+let infos = {
+  recorrente: () => {
+    if (info.recorrente === 'Sim' && info2.recorrente === 'Sim'){
+      return 'Ambos Recorrente';
+    }
+    return 'Apenas uma é recorrente'
+  },
+}
+
+console.log(`
+${info.personagem} e ${info2.personagem}
+${info.origem} e ${info2.origem}
+${info.nota} e ${info2.nota}
+${infos.recorrente()}
+`);
+
+// Exercício 6
+
+
+// Exercício 7
+
+
+// Exercício 8
+
