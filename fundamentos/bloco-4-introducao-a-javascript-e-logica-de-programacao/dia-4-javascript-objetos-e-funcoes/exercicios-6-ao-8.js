033d3c0adbdeea84a09e25a08ecfe29f47b72e9e
André Pestana Silva <atlas.ifpa@gmail.com>
2022-05-23 17:17:03 -0300

    Resolvendo o exercício 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-6-ao-8.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-6-ao-8.js
@@ -8,12 +8,15 @@ let leitor = {
       autor: 'Daniela Kopsch',
       editora: 'Tordesilhas',
     },
+    // Exercício 7
+    {
+      titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+      autor: 'JK Rowling',
+      editora: 'Rocco',
+    }
   ],
 };
 // Exercício 6
 console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama "${leitor.livrosFavoritos[0].titulo}"`);
-
-// Exercício 7
-
 
 // Exercício 8
