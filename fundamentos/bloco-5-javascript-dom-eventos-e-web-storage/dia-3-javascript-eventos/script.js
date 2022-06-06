22246e629bb791497ac4b6dfe81f07a2080f1380
André Pestana Silva <atlas.ifpa@gmail.com>
2022-06-06 14:51:41 -0300

    Resolvendo exercício 1 - Dias do mês

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -13,4 +13,28 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
+// Exercício 1 - Dias do mês
+function createDaysOfTheMonth() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysList = document.querySelector('#days');
+  let countDay = 1;
+  
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    const days = dezDaysList[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.setAttribute('class', 'day');
+    dayListItem.innerText = days;
+    if (countDay === 6) {
+      dayListItem.setAttribute('class', `${dayListItem.getAttribute('class')} friday`);
+      countDay = 0;
+    } else {
+      countDay += 1;
+    }
+    if (days == 24 || days == 25 || days == 31) {
+      dayListItem.setAttribute('class', `${dayListItem.getAttribute('class')} holiday`);
+    }
+    daysList.appendChild(dayListItem);
+  }
+}
+
+createDaysOfTheMonth();
