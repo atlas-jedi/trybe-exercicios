f423a9c97b671fb0287eda07c7adaa5e1ae14598
André Pestana Silva <atlas.ifpa@gmail.com>
2022-07-06 22:01:03 -0300

    4- função assíncrona que simula mensagem do robô Curiosity

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/agora-a-pratica.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/agora-a-pratica.js
@@ -1,12 +1,11 @@
-const getPlanet = () => {
-  const mars = {
-    name: "Mars",
-    distanceFromSun: {
-      value: 227900000,
-      measurementUnit: "kilometers",
-    },
-  };
-  setTimeout(() => console.log("Returned planet: ", mars), 4000);
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
 };
 
-getPlanet(); // imprime Marte depois de 4 segundos
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = () => setTimeout(() => console.log(getMarsTemperature()), messageDelay());
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
