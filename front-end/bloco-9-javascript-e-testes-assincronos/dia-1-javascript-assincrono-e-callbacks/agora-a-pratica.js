0d1dee5528e0b67145ff266b6f6c0f1a4b9e60ef
André Pestana Silva <atlas.ifpa@gmail.com>
2022-07-24 12:38:37 -0300

    6. Adicionando mensagem de erro quando o robô estiver ocupado

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/agora-a-pratica.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/agora-a-pratica.js
@@ -3,7 +3,7 @@ const messageDelay = () => Math.floor(Math.random() * 5000);
 const getMarsTemperature = () => {
   const maxTemperature = 58;
   return Math.floor(Math.random() * maxTemperature);
-};
+}
 
 const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
 
@@ -13,8 +13,22 @@ const temperatureInFahrenheit = (temperature) =>
 const greet = (temperature) =>
   console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
-// crie a função sendMarsTemperature abaixo
-const sendMarsTemperature = (callback) => setTimeout(() => callback(getMarsTemperature()), messageDelay());
+const handleError = (errorReason) =>
+  console.log(`Error getting temperature: ${errorReason}`);
 
-sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
-sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+// definição da função sendMarsTemperature...
+const sendMarsTemperature = (callback, handleError) => {
+  const isCuriosityFree = Math.random() >= 0.4;
+  try {
+    if (!isCuriosityFree) throw Error('Robot is busy').message;
+    setTimeout(() => callback(getMarsTemperature()), messageDelay());
+  } catch (err) {
+    handleError(err);
+  }
+};
+
+// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(greet, handleError);
