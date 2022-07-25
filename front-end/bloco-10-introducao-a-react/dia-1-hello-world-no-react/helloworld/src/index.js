f88474e47775ad60c6fd20904df2fb3eb09d379b
André Pestana Silva <atlas.ifpa@gmail.com>
2022-07-25 18:47:37 -0300

    Exemplo de aplicação com função render

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/helloworld/src/index.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/helloworld/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+// Neste exemplo, estamos chamando a função tick
+// que chama o ReactDOM.render a cada segundo,
+// e injeta no elemento pai com id root um
+// 'Hello World' e o horário.
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString('pt', { hour12: true })}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+setInterval(tick, 1000);
